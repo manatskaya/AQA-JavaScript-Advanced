85d80876994d4b1afda825e5e02b273cc28b656c
manatskaya Svitlana <svetlana.manatskaya@gmail.com>
2023-09-21 12:47:57 +0200

    added variables to lesson 3

---
+++ js-variables/variables.js
@@ -0,0 +1,15 @@
+let adminName = "Admin";
+console.log(adminName);
+console.log(typeof adminName);
+let userName = "First Name";
+userName = "awesome";
+console.log(userName);
+console.log(typeof userName);
+let firstName = "Svitlana";
+console.log(firstName);
+let lastName = "Manatska";
+console.log(lastName);
+console.log( `Javascript is: ${adminName} ${userName} ${firstName} ${lastName}` )
+let age = "10";
+age = 18
+console.log(age);
