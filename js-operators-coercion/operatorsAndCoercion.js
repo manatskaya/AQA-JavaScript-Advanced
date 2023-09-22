efd8261eb3f397eb7066b4fe74aeb2d78053f96f
manatskaya Svitlana <svetlana.manatskaya@gmail.com>
2023-09-22 13:55:13 +0200

    -m "updated my pet project file"

---
+++ js-operators-coercion/operatorsAndCoercion.js
@@ -0,0 +1,16 @@
+// task 1 - compare driving license age and allowance of obtain them
+let driversAge = 20;
+let driversLicenseAge = driversAge >= 18;
+let isDrivingAllowed = true;
+
+console.log(driversLicenseAge && isDrivingAllowed);
+// task 2 - create 2 variables and then make a concatenation
+let userName1 = "Maria";
+
+console.log(userName1);
+
+let userName2 = "Anna";
+
+console.log(userName2);
+
+
