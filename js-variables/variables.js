6a05e86d514c197d4872c572803254039aa9c6cf
manatskaya Svitlana <svetlana.manatskaya@gmail.com>
2023-09-22 12:46:24 +0200

    added punctuation to Javascript line and additional spaces

--- js-variables/variables.js
+++ js-variables/variables.js
@@ -1,17 +1,21 @@
 let adminName = "Admin";
+
 console.log(adminName, typeof adminName);
 
 let userName = "First Name";
 userName = "awesome";
+
 console.log(userName, typeof userName);
 
 let firstName = "Svitlana";
+
 console.log(firstName);
 
 let lastName = "Manatska";
+
 console.log(lastName);
 
-console.log( `Javascript is: ${adminName} ${userName} ${firstName} ${lastName}` )
+console.log( `Javascript is: ${adminName} ${userName} ${firstName} ${lastName}` );
 
 let age = "10";
 age = 18;
