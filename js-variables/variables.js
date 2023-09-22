5f547eb95554bf4e5d2a7b411f1d761f65699db8
Svitlana <svetlana.manatskaya@gmail.com>
2023-09-22 12:49:53 +0200

    added variables to lesson 3 (#2)
    * added variables to lesson 3
    
    * updated code according to a comment provided
    
    * added punctuation to Javascript line and additional spaces

---
+++ js-variables/variables.js
@@ -0,0 +1,22 @@
+let adminName = "Admin";
+
+console.log(adminName, typeof adminName);
+
+let userName = "First Name";
+userName = "awesome";
+
+console.log(userName, typeof userName);
+
+let firstName = "Svitlana";
+
+console.log(firstName);
+
+let lastName = "Manatska";
+
+console.log(lastName);
+
+console.log( `Javascript is: ${adminName} ${userName} ${firstName} ${lastName}` );
+
+let age = "10";
+age = 18;
+console.log(age);
