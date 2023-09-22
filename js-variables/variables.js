7b47edcf4e841059d5b73652e2a378f2ec82f739
manatskaya Svitlana <svetlana.manatskaya@gmail.com>
2023-09-22 12:39:48 +0200

    updated code according to a comment provided

--- js-variables/variables.js
+++ js-variables/variables.js
@@ -1,15 +1,18 @@
 let adminName = "Admin";
-console.log(adminName);
-console.log(typeof adminName);
+console.log(adminName, typeof adminName);
+
 let userName = "First Name";
 userName = "awesome";
-console.log(userName);
-console.log(typeof userName);
+console.log(userName, typeof userName);
+
 let firstName = "Svitlana";
 console.log(firstName);
+
 let lastName = "Manatska";
 console.log(lastName);
+
 console.log( `Javascript is: ${adminName} ${userName} ${firstName} ${lastName}` )
+
 let age = "10";
-age = 18
+age = 18;
 console.log(age);
